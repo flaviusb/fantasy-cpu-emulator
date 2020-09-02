2e2fefe74079d62918ec69e4a93e3fb2550bc637
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2020-09-03 01:05:27 +1200

    Start adding info to the instructions.

--- fantasy-cpu-emulator-macros/src/lib.rs
+++ fantasy-cpu-emulator-macros/src/lib.rs
@@ -179,15 +179,27 @@ pub fn define_chip(input: TokenStream) -> TokenStream {
 
   let chip_info: ChipInfo = syn::parse(input).unwrap();
   let mod_name = format_ident!("{}", chip_info.name);
-  let instruction_seq: syn::punctuated::Punctuated<syn::Variant, Token![,]> = chip_info.instructions.instructions.into_iter().map(|instr| {
+  let instruction_seq: syn::punctuated::Punctuated<syn::Variant, Token![,]> = chip_info.instructions.instructions.iter().map(|instr| {
     let name = quote::format_ident!("{}", instr.name);
     let v: syn::Variant = syn::parse_quote! {
-      #name()
+      #name(Instructions::#name)
+    };
+    v
+  }).collect();
+  let instruction_structs: Vec<syn::ItemStruct> = chip_info.instructions.instructions.into_iter().map(|instr| {
+    let name = quote::format_ident!("{}", instr.name);
+    let v: syn::ItemStruct = syn::parse_quote! {
+      #[derive(Debug,PartialEq,Eq)]
+      pub struct #name {
+      }
     };
     v
   }).collect();
   (quote! {
     mod #mod_name {
+      pub mod Instructions {
+        #(#instruction_structs)*
+      }
       #[derive(Debug,PartialEq,Eq)]
       pub enum Instruction {
         #instruction_seq
