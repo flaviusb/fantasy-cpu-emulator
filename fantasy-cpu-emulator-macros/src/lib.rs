ba64f929b6d2c5ef97c77e268edb132ab1f0da53
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2020-10-13 19:22:38 +1300

    Skip stage parsing if we don't have the right kind of closure.

--- fantasy-cpu-emulator-macros/src/lib.rs
+++ fantasy-cpu-emulator-macros/src/lib.rs
@@ -101,7 +101,7 @@ impl Parse for Instruction {
       let stage_packed = input.parse::<syn::ExprClosure>()?;
       let (ast_frag_name, ast_frag_args, stage_action) = match stage_packed {
         syn::ExprClosure{attrs:_, asyncness: None, movability: None, capture: None, or1_token: syn::token::Or{..}, inputs: inputs, or2_token: syn::token::Or{..}, output: syn::ReturnType::Type(syn::token::RArrow{..}, name), body: body} => ((),(),()),
-        _ => ((),(),()),
+        _ => {input.parse::<Token![,]>()?; continue;},
       };
       input.parse::<Token![,]>()?;
     }
