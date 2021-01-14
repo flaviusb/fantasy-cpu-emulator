6743558d813677af24cc24ead2608bf70254b1de
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2021-01-15 00:59:01 +1300

    Add imports to emulator to remind me.

--- src/bin/fantasy_emulator.rs
+++ src/bin/fantasy_emulator.rs
@@ -1,4 +1,11 @@
 use fantasy_cpu_emulator::jackfruit;
 
+use getopts::{Options,Matches};
+use std::{env,process};
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+
 fn main() {
 }
